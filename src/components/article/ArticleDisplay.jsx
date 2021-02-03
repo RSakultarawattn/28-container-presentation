698fd9067207ce275444c7bd3cf64c19f9c62e72
RSakultarawattn <ray.sakultarawattn@gmail.com>
2021-02-03 13:31:40 -0800

    article display

---
+++ src/components/article/ArticleDisplay.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ArticleDisplay = ({ title, author, description }) => (
+  <div
+    data-testid="display"
+    style={{ color: title, backgroundColor: author, width:'50rm', height: '50rm' }}>
+    <p style={{ fontsize: '5rem' }}>{description}</p>
+  </div>
+);
+
+ArticleDisplay.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired
+};
+
+export default ArticleDisplay;
