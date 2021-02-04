76e186a5e1a5ec515561bbed280a0b31077bd1a2
RSakultarawattn <ray.sakultarawattn@gmail.com>
2021-02-04 14:37:28 -0800

    search test passing

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -20,18 +20,3 @@ Article.propTypes = {
 export default Article;
 
 
-// import React from 'react';
-// import PropTypes from 'prop-types';
-
-// export default function Article({ article }) {
-//   return (
-//     <div>
-//       <h1>{article.title}</h1>
-//       <p>{article.author}</p>
-//       <p>{article.content}</p>
-//     </div>
-//   );
-// }
-// Article.propTypes = {
-//   article: PropTypes.object,
-// };
--- src/components/search/Searchinput2.test.jsx
+++ src/components/search/Searchinput2.test.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
-import App from './App';
+import Searchinput2 from './Searchinput2';
 
-describe('App component', () => {
+const search = '';
+function handleChange(){
+
+}
+function handleSearch() {
+
+}
+
+describe('search input', () => {
   afterEach(() => cleanup());
   it('renders App', () => {
-    const { asFragment } = render(<App />);
+    const { asFragment } = render(<Searchinput2 search={search} 
+      handleChange={handleChange} 
+      onSubmit={handleSearch} />);
     expect(asFragment()).toMatchSnapshot();
   });
 });
